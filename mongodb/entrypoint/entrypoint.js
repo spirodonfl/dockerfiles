6e9d232acc3c097f282239dc5887ef8f1f11cfc5
Spiro Floropoulos <spirodonfl@gmail.com>
2018-12-08 15:04:23 -0800

    Adding custom users on build/init

---
+++ mongodb/entrypoint/entrypoint.js
@@ -0,0 +1,17 @@
+var db = connect("mongodb://localhost/admin");
+
+// db.createUser(
+//     {
+//         user: "yourAdminUserName",
+//         pwd: "yourAdminPassword",
+//         roles: [ { role: "userAdminAnyDatabase", db: "admin" } ]
+//     }
+// );
+
+// db.createUser(
+//     {
+//         user: "yourAdminUserName",
+//         pwd: "yourAdminPassword",
+//         roles: [ { role: "readAnyDatabase", db: "admin" } ]
+//     }
+// );
